a082468e2ae5b99b9581c891e398943f30aee6b8
furecool <furecool@gmail.com>
2020-10-21 23:27:01 +0800

    更新 function count()

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,12 @@
-// toggle active
+// checkbox
+function check_all(obj,cName){
+    var checkboxs = document.getElementsByName(cName);
+    for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;}
+}
+
 $(function () {
 
+  // toggle active
   $(function(){
     $("h3").click(function(){
       var idx=$(this).index();
@@ -10,11 +16,58 @@ $(function () {
     });
   });
 
-});
+  // remove appendTo
+  $(".tbody").on("click", ".remove", function () {
+    $(this).parent().parent().parent().remove();
+    count();
+  });
 
-// checkbox
-function check_all(obj,cName)
-{
-    var checkboxs = document.getElementsByName(cName);
-    for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;}
-}
+  $(".tbody").on("click", ".to-wish", function () {
+    $(this).parent().parent().parent().appendTo(".wish .tbody");
+    $(this).text("放入購物車").removeClass("to-wish").addClass("to-shopping");
+    count();
+  });
+
+  $(".tbody").on("click", ".to-shopping", function () {
+    $(this).parent().parent().parent().appendTo(".shopping .tbody");
+    $(this).text("放入願望清單").removeClass("to-shopping").addClass("to-wish");
+    count();
+  });
+
+  $(".wish").on("click", ".all-to-shopping", function () {
+    $(".wish .tbody ul").appendTo(".shopping .tbody");
+    $(".to-shopping").text("放入願望清單").removeClass("to-shopping").addClass("to-wish");
+    count();
+  });
+
+  // count
+
+  count();
+
+  function count(){
+
+    // console.log($(".shopping .count").eq(0).text());
+    // console.log($(".shopping .tbody .li-3 input").eq(0).val());
+
+    var total = 0;
+    $(".shopping .count").each(function(){
+       total += parseInt($(this).text());
+    });
+    // console.log(total);
+    $(".all-count").text(total);
+
+
+    var amount = 0;
+    $(".shopping .tbody .li-3 input").each(function(){
+      amount += parseInt($(this).val());
+    });
+    // console.log(amount);
+    $(".prod").text(amount);
+
+  }
+
+
+
+
+
+});
